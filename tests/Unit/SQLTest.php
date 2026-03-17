18a0567fc5109b0973d07f2ae5f33bf2409ab973
eribys <erikas.bystautas@tutotoons.com>
2026-03-17 05:52:08 +0000

    add more tests

--- tests/Unit/SQLTest.php
+++ tests/Unit/SQLTest.php
@@ -1,6 +1,27 @@
 <?php
 
 use ByErikas\EloquentBigQuery\Builder;
+use ByErikas\EloquentBigQuery\Facades\MetricsRepository;
+use ByErikas\EloquentBigQuery\Facades\QueryService;
+use ByErikas\EloquentBigQuery\Join;
+use ByErikas\EloquentBigQuery\Where;
+
+it("can set and get client config", function () {
+    QueryService::setClientConfig(["key" => true]);
+
+    expect(QueryService::getClientConfig())->toBe(["key" => true]);
+});
+
+it("can set metrics to repository and get metrics from it", function () {
+    MetricsRepository::from([["keyword" => "test", "value" => "SUM(*)"]]);
+
+    expect(MetricsRepository::all())->toBe([["keyword" => "test", "value" => "SUM(*)"]]);
+
+    expect(MetricsRepository::find("test"))->toBe([
+        "keyword" => "test",
+        "value" => "SUM(*)"
+    ]);
+});
 
 it("can generate basic select from SQL with dates, offset, limit and aliases", function () {
     $start = now();
@@ -19,4 +40,26 @@ it("can generate basic select from SQL with dates, offset, limit and aliases", f
         ->toSQL();
 
     expect($sql)->toBe("SELECT column AS column_alias FROM `test` table_alias WHERE columnA = \"value\" AND columnB IN (1, '1', 'yes') AND columnC BETWEEN \"1000-01-01\" AND \"2000-01-01\" AND columnD BETWEEN \"{$start->format("Y-m-d H:i:s")}\" AND \"{$end->format("Y-m-d H:i:s")}\" GROUP BY column_alias ORDER BY column_alias DESC LIMIT 10 OFFSET 5");
+});
+
+it("can generate advanced wheres", function () {
+    $sql = Builder::table("test")
+        ->select(["column"])
+        ->where(function (Where $query) {
+            $query->whereIn("columnA", [1, 2, 3])
+                ->whereBetween("columnB", "1000-01-01", "2000-01-01", "or");
+        })->toSQL();
+
+    expect($sql)->toBe("SELECT column FROM `test` WHERE (columnA IN (1, 2, 3) OR columnB BETWEEN \"1000-01-01\" AND \"2000-01-01\")");
+});
+
+it("can generate join", function () {
+    $sql = Builder::table("test")
+        ->select(["column"])
+        ->leftJoin("test2", "t2", function (Join $query) {
+            $query->where("time", "test.time")
+                ->whereBetween("time", "1000-01-01", "2000-01-01");
+        })->toSQL();
+
+    expect($sql)->toBe("SELECT column FROM `test` LEFT JOIN `test2` t2 ON t2.time = test.time AND t2.time BETWEEN \"1000-01-01\" AND \"2000-01-01\"");
 });
