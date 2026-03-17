b564c308232d837d1e266860389ba51dd6bf1f9e
eribys <erikas.bystautas@tutotoons.com>
2026-03-17 06:22:10 +0000

    more test fixes

--- tests/Unit/SQLTest.php
+++ tests/Unit/SQLTest.php
@@ -53,7 +53,7 @@ it("can generate advanced wheres", function () {
 });
 
 it("can generate join and subquery", function () {
-    MetricsRepository::from(collect(["keyword" => "test", "value" => "SUM(*)"]));
+    MetricsRepository::from(collect([["keyword" => "test", "value" => "SUM(*)"]]));
 
     $queryA = Builder::table("test")
         ->select(["column"])
